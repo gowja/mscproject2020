ab9fc8d7f65a1694732ec1e06a4cb510a905fad2
gowja <gowjavijayakumaran@gmail.com>
2020-08-09 22:08:15 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DeptInternalComponent } from './dept-internal/dept-internal.component';
+import { InternalComponent } from './internal/internal.component';
+import { CampExternalComponent } from './camp-external/camp-external.component';
+import { ExternalComponent } from './external/external.component';
+import { PbComponent } from './pb/pb.component';
+import { PbviewComponent } from './pbview/pbview.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dept',component:DeptInternalComponent,
+  children:[
+    {path:"internal",component:InternalComponent}
+  ]},
+  {path:'camp',component:CampExternalComponent,
+  children:[
+    {path:'external',component:ExternalComponent}
+  ]},
+  {path:'pb',component:PbComponent,
+  children:[
+    {path:'pbview',component:PbviewComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SloginComponent } from './slogin/slogin.component';
+import { DeptInternalComponent } from './dept-internal/dept-internal.component';
+import { InternalComponent } from './internal/internal.component';
+import { CampExternalComponent } from './camp-external/camp-external.component';
+import { ExternalComponent } from './external/external.component';
+import { PbComponent } from './pb/pb.component';
+import { PbviewComponent } from './pbview/pbview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SloginComponent,
+    DeptInternalComponent,
+    InternalComponent,
+    CampExternalComponent,
+    ExternalComponent,
+    PbComponent,
+    PbviewComponent
   ],
   imports: [
     BrowserModule,
